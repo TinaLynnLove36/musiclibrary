cf4e82f4abc2a9ceb4e8a931a4a1720e7e25772a
TinaLynnLove36 <fartinasegura@gmail.com>
2021-04-15 18:46:35 -0500

    First Commit

---
+++ src/Components/Search.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Search extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>this is my search component</h1>
+            </div>
+        )
+    }
+}
+
+export default Search
